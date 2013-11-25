9d389532688830137f7b468d3b162fb5b5ce78dc
angel_cereijo <angel.cereijo@medianet.es>
2013-11-25 12:25:21 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 srt
 ===
 
-Plugin to search tweet for a defined account in Twitter
+Chrome plugin to allow search tweet in a defined account and show result in a new tab
+
+* Chrome plugin page: https://developer.chrome.com/extensions/ugtur/insertionQuery
---
+++ srt.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+	var labelForAccount = chrome.i18n.getMessage("placeholderForAccount");
+	var labelForText = chrome.i18n.getMessage("placeholderForText");
+	var buttonSearch = chrome.i18n.getMessage("buttonSearch");
+
+	$('#account').attr('placeholder',labelForAccount);
+	$('#text').attr('placeholder',labelForText);
+	$('#search').val(buttonSearch);
+
+	chrome.storage.local.get('account', function(data) {
+      if (data.account)
+        $('#account').val(data.account);
+    });
+
+	$('#search').click(function(){
+		var account = $('#account').val(),
+			text = $('#text').val();
+		if(account!='' && text!=''){
+			chrome.storage.local.set({account: account});
+			var newURL = "https://twitter.com/search?q="+text+"%20from%3A"+account+"&src=typd";
+    		chrome.tabs.create({ url: newURL });
+		}
+	});
+
+});
