03595ace152c948d1f511faef54a13aafbf2873d
angel_cereijo <angel.cereijo@medianet.es>
2013-12-03 16:14:49 +0100

    change code to execute two script and try to work in MAC

--- srt.js
+++ srt.js
@@ -1,46 +1,59 @@
+//var ExternFunction = 
+/*(function() {
 
-$(document).ready(function () {
-	var labelForAccount = chrome.i18n.getMessage("placeholderForAccount"),
-		labelForText = chrome.i18n.getMessage("placeholderForText"),
-		buttonSearch = chrome.i18n.getMessage("buttonSearch"),
-		accountList=[],
-		addElement = function ( elem ) {
-		    accountList.push( elem );
-		};
+        //public methods
+        return {
+        	initPlugin : function () {
+		alert('dentroq:'+accountList+","+account+","+text);	
+					if(account!='' && text!=''){
+					chrome.storage.local.set({account: account});
+					alert('dentro3');
+					if(!jQuery.inArray( account, accountList )){
+						alert('dentro4');
+						accountList.push(account);
+						alert('dentro5:'+accountList);
+						chrome.storage.local.set({accountList: accountList});
+						alert('dentro6');
+					}
+					alert(accountList);
+					var includeRetweets = "%20include%3Aretweets";
+					var newURL = "https://twitter.com/search?q="+text+"%20from%3A"+account+includeRetweets+"&src=typd";
+		    		//chrome.tabs.create({ url: newURL });
+		    		//window.open(newURL);
+		    		//return newURL;
+				}
+            }
+        }
+})//();
+/*
+alert("eu");
+//execute when load
+ExternFunction.initPlugin();
+alert("eu");*/
 
-	$('#account').attr('placeholder',labelForAccount);
-	$('#text').attr('placeholder',labelForText);
-	$('#search').val(buttonSearch);
-
-	chrome.storage.local.get('account', function(data) {
-      if (data.account) {  $('#account').val(data.account); }
-    });
-	chrome.storage.local.get('accountList', function(data) {
-      	if (data.accountList){
-      		data.accountList.forEach( addElement );
-    		alert(accountList);
-    	}
-    });
-	
-	$( "#account" ).autocomplete({
-      source: accountList
-    });
-
-	$('#search').click(function(){
-		var account = $('#account').val(),
-			text = $('#text').val();
-		if(account!='' && text!=''){
-			chrome.storage.local.set({account: account});
-			if(!jQuery.inArray( account, accountList )){
-				accountList.push(account);
-				chrome.storage.local.set({accountList: accountList});
-			}
-			alert(accountList);
 			
-			var includeRetweets = "%20include%3Aretweets";
-			var newURL = "https://twitter.com/search?q="+text+"%20from%3A"+account+includeRetweets+"&src=typd";
-    		chrome.tabs.create({ url: newURL });
+	if(account!='' && text!=''){
+		chrome.storage.local.set({account: account});
+		alert('dentro3');
+		if(!jQuery.inArray( account, accountList )){
+			alert('dentro4');
+			accountList.push(account);
+			alert('dentro5:'+accountList);
+			chrome.storage.local.set({accountList: accountList});
+			alert('dentro6');
 		}
-	});
+		alert(accountList);
+		var includeRetweets = "%20include%3Aretweets";
+		newURL = "https://twitter.com/search?q="+text+"%20from%3A"+account+includeRetweets+"&src=typd";
+		//chrome.tabs.create({ url: newURL });
+		//window.open(newURL);
+		//newURL;
+		chrome.runtime.sendMessage({
+            type: 'result',
+            name: 'url',
+            value: newURL
+        });
 
-});
+        alert('fin');
+	}
+			
---
+++ srt_public.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+       
+var labelForAccount = chrome.i18n.getMessage("placeholderForAccount"),
+		labelForText = chrome.i18n.getMessage("placeholderForText"),
+		buttonSearch = chrome.i18n.getMessage("buttonSearch"),
+		accountList=[],
+		addElement = function ( elem ) {
+		    accountList.push( elem );
+		};
+
+	$('#account').attr('placeholder',labelForAccount);
+	$('#text').attr('placeholder',labelForText);
+	$('#search').val(buttonSearch);
+
+	//alert("initPlugin- account:"+$('#account')+", text:"+$('#text')+",search:"+$('#search'));
+
+	chrome.storage.local.get('account', function(data) {
+      if (data.account) {  $('#account').val(data.account); }	
+    });
+	chrome.storage.local.get('accountList', function(data) {
+      	if (data.accountList){
+      		data.accountList.forEach( addElement );
+    		//alert(accountList);
+    	}
+    });
+	
+	$( "#account" ).autocomplete({
+      source: accountList
+    });
+
+	$('#search').click(function(){
+		
+		var account = $('#account').val(),
+					text = $('#text').val(),newURL='a';
+		alert('lanza:'+account+","+text);
+		chrome.tabs.executeScript(null, {file: "jquery-1.9.1.min.js"}, function(){
+		    chrome.tabs.executeScript(null, {code: "var accountList = '"+accountList+"',account='"+account+"',text='"+text+"';"}, function(){
+		        chrome.tabs.executeScript(null, {file: "srt.js"}, function(){ 
+		        });
+		    });
+		});
+		alert('lanzado');
+	});
+
+	chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+	    if (request.type == 'result') {
+	        chrome.tabs.create({ url: request.value });
+	    }
+	});
+
+        
+});
